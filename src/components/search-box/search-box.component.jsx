68e726afb320620873a55f3a11484c32f44739ca
Sohrabi Sasha <shahab.sohrabi@mimacom.com>
2024-05-15 12:41:07 +0200

    React basics: convert components to functional components

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,14 @@
-import { Component } from 'react';
 import './search-box.styles.css';
 
-class SearchBax extends Component {
-  render() {
-    return (
-      <input
-        className={`search-box ${this.props.className}`}
-        type="search"
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler}
-      />
-    );
-  }
-}
+const SearchBax = ({ className, placeholder, onChangeHandler }) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  );
+};
 
 export default SearchBax;
